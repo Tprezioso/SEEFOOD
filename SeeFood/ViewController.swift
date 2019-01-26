1c5585758a8f1f43d2612664e0f094659292c552
Tprezioso <tommyprezioso@gmail.com>
2019-01-26 18:21:39 -0500

    setup app design

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var displayImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    
+    }
+    
 }
 
