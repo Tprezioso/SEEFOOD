9de30f0921e3a9e09f63ba280741e721645ed58b
Tprezioso <tommyprezioso@gmail.com>
2019-01-27 20:12:31 -0500

    added recognition to hotdog not hotdog

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -28,10 +28,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             displayImageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResults = results.first {
+                if firstResults.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog"
+                } else {
+                    self.navigationItem.title = "Not Hotdog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try! handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
